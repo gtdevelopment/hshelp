9c75186e341d2d266af42f3edf31e7ae9bb0f10d
gtdevelopment <32522851+gtdevelopment@users.noreply.github.com>
2018-06-19 03:46:19 +0200

    Add files via upload

---
+++ gt-hellosign.php
@@ -0,0 +1,14 @@
+
+<?php
+/**
+Plugin Name: GT Hellosign
+description: >-
+  GT's Hellosign integration
+Version: 1.0
+Author: The Guarantree LLC
+License: GPLv2 or later
+Text Domain: gt-hellosign
+*/
+
+
+require_once __DIR__ . '/HelloSign.php';
